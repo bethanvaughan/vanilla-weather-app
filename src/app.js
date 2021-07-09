55f8a91cf79bfa2f848fa12fd2379f392ed9c9e2
bethanvaughan <86553590+bethanvaughan@users.noreply.github.com>
2021-07-09 17:25:03 +0100

    Added axios, api key and api url

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-alert("Hello World");
+let apiKey = "c6f1a8e379608edf502b9056c45c42ed";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+
+console.log(apiUrl);
