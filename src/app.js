9ac623955b8c2b50c806df20ceea148242e00d56
bethanvaughan <86553590+bethanvaughan@users.noreply.github.com>
2021-07-10 12:51:37 +0100

    Showing the current temperature and weather description

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temmperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  cityElement.innerHTML = response.data.name;
+  temmperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
 let apiKey = "c6f1a8e379608edf502b9056c45c42ed";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cambridge&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then((response) => {
   console.log(response);
 });
+
+axios.get(apiUrl).then(displayTemperature);
