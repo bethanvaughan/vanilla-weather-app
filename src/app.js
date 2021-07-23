9ca5e9bca6aa5141b496965b47d078fb7eea4bed
bethanvaughan <86553590+bethanvaughan@users.noreply.github.com>
2021-07-23 15:13:47 +0300

    Addded forecast id in html

--- src/app.js
+++ src/app.js
@@ -22,6 +22,12 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML= "hi";
+}
+
 function displayTemperature(response) {
   let temmperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +92,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 search("London");
+
