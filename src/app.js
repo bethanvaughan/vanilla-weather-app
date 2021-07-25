7de1ac8b0388c51af4cc70ae9c5e2de9dd343e6d
bethanvaughan <86553590+bethanvaughan@users.noreply.github.com>
2021-07-25 12:24:14 +0300

    Displayed the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -22,28 +22,39 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+let date = new Date(timestamp *1000);
+let day = date.getDay();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+return days[day]; 
+
+}
+
 
 function displayForecast(response) {
-    console.log(response.data.daily);
-    let forecastElement = document.querySelector("#forecast");
+let forecast = response.data.daily;
 
-let forecastHTML = `<div class="row"> `;
-let days = ["Thu", "Fri", "Sat", "Sun"];
-days.forEach(function(day) {
+
+let forecastElement = document.querySelector("#forecast");
+
+let forecastHTML = `<div class="row">`;
+forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
 forecastHTML = forecastHTML + `
     
 <div class="col-2"> 
 <div class="weather-forecast-date"> 
-${day}
+${formatDay(forecastDay.dt)}
 </div>
-<img src="http://openweathermap.org/img/wn/01n@2x.png" width="36"/>
+<img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" width="36"/>
 
 <div class="forecast-temperature"> 
-  12, 18
+  ${Math.round(forecastDay.temp.max)}° ${Math.round(forecastDay.temp.min)}°
 </div>
  </div> 
 </div>  
-    `;
+    `; }
 })
 
 
