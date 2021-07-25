3c601dd1d4a26c63b82e8c209cf80fc1abcd85c8
bethanvaughan <86553590+bethanvaughan@users.noreply.github.com>
2021-07-25 20:56:59 +0300

    Updated spacing in css

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp){
 let date = new Date(timestamp *1000);
 let day = date.getDay();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 return days[day]; 
 
@@ -51,7 +51,7 @@ ${formatDay(forecastDay.dt)}
 <div class="forecast-temperature"> 
   ${Math.round(forecastDay.temp.max)}° / ${Math.round(forecastDay.temp.min)}°
 </div>
- </div> 
+  
 </div>  
     `; }
 })
